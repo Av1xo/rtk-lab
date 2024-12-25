b3bc25fe14ab82f86662dac0eac0a7a76d73732c
Gw1xo <rostikzhurbenko6@gmail.com>
2024-12-25 23:43:02 +0200

    board

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Board from './components/Board/Board';
 
 const App = () => {
 
   return (
     <>
+      <div className='App'>
+        <Board />
+      </div>
     </>
   );
 };
